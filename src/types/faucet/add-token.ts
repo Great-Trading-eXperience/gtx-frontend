43dfe4371c641ee75faf2d37f8412d22128201bb
hmiftachul11 <hmiftachul11@gmail.com>
2025-10-19 11:25:48 +0700

    feat: add faucet address and update types faucet token

--- src/types/faucet/add-token.ts
+++ src/types/faucet/add-token.ts
@@ -3,6 +3,8 @@ import { HexAddress } from "../general/address";
 export default interface FaucetToken {
   id: string;
   token: HexAddress;
+  symbol: string;
+  decimals: number;
   blockNumber: number;
   blockTimestamp: number;
   transactionHash: string;
